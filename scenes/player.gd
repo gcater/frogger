40c51de2fc59d112090ffdcb3d60a181cc5b3fa2
gcater1 <geoffcater123@gmail.com>
2025-11-11 17:37:06 -0500

    add scaling and movement

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,11 +1,7 @@
 extends Node2D
 
+var direction : Vector2 = Vector2(1,0)
+var speed: int = 5
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	position += direction * speed
